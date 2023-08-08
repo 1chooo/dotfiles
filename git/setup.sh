3c5dcfc0937ff9f2e23aded99a2456601d52ee6d
Hugo ChunHo Lin <hugo970217@gmail.com>
2023-08-08 11:51:43 +0800

    Update git/setup.sh

--- git/setup.sh
+++ git/setup.sh
@@ -1,7 +1,28 @@
+#!/usr/bin/env bash
+#   Version v0.0.1
+#   Author: Hugo ChunHo Lin
+#   GitHub: github.com/1chooo
+#   Copyright (C) 2023 Hugo ChunHo Lin All rights reserved.
+#
+
+CurrentUser=$1
+HomeDirectory=$2
+ScriptLocation=$3  # root folder setup.sh location
+
+function initial()
+{
+    if [ "$CurrentUser" == "" ] || [ "$HomeDirectory" == "" ] || [ "$ScriptLocation" == "" ]; then
+        echo "[Git Setup] No argument passed"
+        exit 1
+    fi
+    ScriptLocation="$ScriptLocation/git"
+}
+
 git_email="hugo970217@gmail.com"
 git_name="1chooo"
 
-echo "set git environment settings..."
+echo "ready to set git environment settings..."
 git config --global user.email $git_email
 git config --global user.name $git_name
 git config --global color.ui true
+git config --global pull.rebase false
